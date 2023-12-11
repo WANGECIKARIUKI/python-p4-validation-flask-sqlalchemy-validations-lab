e4fa9324109c62f0d43ce2a43aa18746d93c648b
LUCY <wangecikariu.luc@gmail.com>
2023-12-11 14:40:16 +0300

    finished lab

--- server/debug.py
+++ server/debug.py
@@ -9,3 +9,4 @@ if __name__ == '__main__':
     
     with app.app_context():
         import ipdb; ipdb.set_trace()
+        from models import db, Author, Post
--- server/models.py
+++ server/models.py
@@ -6,12 +6,32 @@ class Author(db.Model):
     __tablename__ = 'authors'
     
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, nullable=False, unique=True)
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    # Add validators
+    @validates("name")
+    def validate_name(self, key, value):
+        if not value:
+            raise ValueError("Enter a valid name")
+        return value
+        #elif Author.query.filter_by(name=value).first():
+            #raise ValueError("That author already exists!")
+        #else:
+            #return value
+    #@validates('author')
+    #def validate_name(self, key, author):
+       # if author is not None:
+           # raise ValueError("No two authors have the same name.")
+        #return author
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError("Phone number must be 10 digits.")
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,7 +47,31 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Title field is required.")
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(substring in title for substring in clickbait):
+            raise ValueError("No clickbait found")
+        return title
+
+    @validates('content', 'summary')
+    def validate_length(self, key, string):
+        if( key == 'content'):
+            if len(string) < 250:
+                raise ValueError("Post content must be greater than or equal 250 characters long.")
+        if( key == 'summary'):
+            if len(string) > 250:
+                raise ValueError("Post summary must be less than or equal to 250 characters long.")
+        return string
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category != 'Fiction' and category != 'Non-Fiction':
+            raise ValueError("Category must be Fiction or Non-Fiction.")
+        return category 
 
 
     def __repr__(self):
